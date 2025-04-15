875ee98e9de963fb3273ee5b749febeebae2b2a1
ketanjinak <97indra.s@gmail.com>
2025-04-16 01:21:14 +0700

    Add files via upload

---
+++ tugas pertemuan.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	string nama,kelas,nim,umur,hobby;
+		cout << "Masukan Nama:";
+		cin >> nama;
+		cout << "Masukan kelas:";
+		cin >> kelas;
+		cout << "Masukan NIM:";
+		cin >> nim;
+		cout << "Masukan Umur:";
+		cin >> umur;
+		cout << "Masukan Hobby:";
+		cin >> hobby;
+	
+		cout << "halo perkenalkan nama saya "<<nama<<endl;
+		cout << "dari kelas "<<kelas; 
+		cout << " dengan NIM "<<nim<<endl;
+		cout << "saat ini saya menginjak umur "<<umur<<endl;
+		cout << "hobby saya "<<hobby;
+		cout << ", saya sangat suka sekali dengan "<<hobby;
+		
+		
+	
+}
